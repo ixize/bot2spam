ded3fd2971c573103351ab3843fca929cdb59967
ixize <55327417+ixize@users.noreply.github.com>
2019-09-18 06:35:44 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const fs = require('fs');
+
+
+
+client.login('NjIyNDczMDUzMzE3NjI3OTE0.XX_RKA.rkcC6W7RA8NCJanr-CxmX-9WcBo');
